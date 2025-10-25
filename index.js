6146cee968f30a05f86093c17f4948db9a883bc3
MasadRayan <masadrayan2002@gmail.com>
2025-10-26 02:35:39 +0600

    Cars post api added

--- index.js
+++ index.js
@@ -2,14 +2,61 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+
 const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json());
 
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.df8vtvh.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const db = client.db("rentifyDB");
+
+    const carsCollection = db.collection("cars");
+    const usersCollection = db.collection("users");
+
+    app.get ('/cars', async (req, res) => {
+      const result = await carsCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.post('/cars', async (req, res) => {
+      const car = req.body;
+      const result = await carsCollection.insertOne(car);
+      res.send(result);
+    })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+run().catch(console.dir);
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!!!!!!!!!!!!!!!!!!!!!')
 })
 
 app.listen(port, () => {
