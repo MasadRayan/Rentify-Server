bad4d0c2597b901d41bfcd8edd30692b9d82531e
MasadRayan <masadrayan2002@gmail.com>
2025-10-28 14:41:58 +0600

    Verify firebase token added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
+const admin = require("firebase-admin");
 
 const port = process.env.PORT || 5000
 
@@ -10,10 +11,14 @@ app.use(cors())
 app.use(express.json());
 
 
+const serviceAccount = require("./firebase-admin_Key.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.df8vtvh.mongodb.net/?appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -33,16 +38,36 @@ async function run() {
     const carsCollection = db.collection("cars");
     const usersCollection = db.collection("users");
 
+    // Custom middleware_______________________________
+    // firebase token
+    const verifyFBToken = async (req, res, next) => {
+      const authHeader = req.headers.authorization;
+      if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(401).send({ message: 'Unauthorized access' });
+      }
+      const idToken = authHeader.split(' ')[1];
+      if (!idToken) {
+        return res.status(401).send({ message: 'Unauthorized access' });
+      }
+
+      // Verify the ID token using Firebase Admin SDK
+      try {
+        const decodedToken = await admin.auth().verifyIdToken(idToken);
+        req.decoded = decodedToken;
+        next();
+      } catch (error) {
+        return res.status(403).send({ message: 'Forbidden access' });
+      }
+
+    }
+
     // Car APIs_______________________________________
 
-    app.get('/cars', async (req, res) => {
-      const result = await carsCollection.find().toArray();
-      res.send(result);
-    })
-
     // GET all cars OR cars by user email
-    app.get('/myCars', async (req, res) => {
+    app.get('/myCars', verifyFBToken, async (req, res) => {
       const email = req.query.email;
+
+      // console.log("Header in cars", req.headers)
 
       if (email) {
         // Get cars by specific user email
@@ -72,12 +97,18 @@ async function run() {
 
     // User APIs_______________________________________
     app.post('/users', async (req, res) => {
+      const email = req.body.email;
+
+      const userExist = await usersCollection.findOne({ email })
+      if (userExist) {
+        return res.status(409).send({ message: "User already exists" });
+      }
       const user = req.body;
       const result = await usersCollection.insertOne(user);
       res.send(result);
     })
 
-    app.get('/users', async (req, res) => {
+    app.get('/users', verifyFBToken, async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
     })
@@ -88,7 +119,6 @@ async function run() {
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
   }
 }
 run().catch(console.dir);
